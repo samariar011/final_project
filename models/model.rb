d4bfd9e6648e4497f6ae1e920f2cae4582d9e1f1
KhadijaHAnif <18khanif@unis.org>
2015-07-02 14:41:28 +0000

    models

---
+++ models/model.rb
@@ -0,0 +1,32 @@
+# class FlowFitness
+  
+#   attr_accessor :lose_fat, :gain_muscle, :gender, :age, :weight
+  
+#   def initialize(lose_fat = 0, gain_muscle = 0, gender, age, weight)
+#     @lose_fat = lose_fat
+#     @gain_muscle = gain_muscle
+#     @gender = gender   
+#     @age = age
+#     @weight = weight
+#   end
+
+class Male
+  
+  attr_accessor :age, :lose_fat, :gain_muscle
+  
+  def initialize(lose_fat = 0, gain_muscle = 0, age)
+    @lose_fat = lose_fat
+    @gain_muscle = gain_muscle
+    @age = age
+  end
+  
+
+    
+    
+    
+    
+    
+    
+    
+    
+  end
